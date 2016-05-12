a33b4429ba617e970e7bcfb53b81885bb7d36c62
Brayden Traas <btraas@gmail.com>
2016-05-11 23:26:50 -0700

    new links page

---
+++ links.php
@@ -0,0 +1,12 @@
+<?php
+$dir_open = opendir('.');
+
+while(false !== ($filename = readdir($dir_open))){
+	    if($filename != "." && $filename != ".."){
+			        $link = "<a href='./$filename'> $filename </a><br />";
+					        echo $link;
+							    }
+}
+
+closedir($dir_open);
+?>
