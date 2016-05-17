0d519ce7e8c6e5f5951e1f00dfeff22b6d7e4cd5
Shawn Kim <sdf1342@gmail.com>
2016-05-17 16:16:12 -0700

    console

--- js/checkandmark.js
+++ js/checkandmark.js
@@ -2,9 +2,12 @@ function showCross(){
 	$('body').append('<img id="something" src="resources/images/cross_mark.png" alt="cross" width="100px" height="100px">');
 
 	var div = $('#something');
-
+	console.log("cross");
     div.animate({opacity: '0.2'}, "slow");
 	div.animate({opacity: '1.0'}, "slow");
+
+	sleep(2);
+
 	div.remove();
 }
 
@@ -12,9 +15,11 @@ function showCheck(){
 	$('body').append('<img id="something" src="resources/images/check_mark.png" alt="cross" width="100px" height="100px">');
 
 	var div = $('#something');
-
+	console.log("check");
     div.animate({opacity: '0.2'}, "slow");
 	div.animate({opacity: '1.0'}, "slow");
-	
+
+	sleep(2);
+
 	div.remove();
 }
