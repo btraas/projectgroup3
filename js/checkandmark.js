863d8bddb048b231c133229265fee9c5e31da9bc
Shawn Kim <sdf1342@gmail.com>
2016-05-17 16:10:40 -0700

    add remove()

--- js/checkandmark.js
+++ js/checkandmark.js
@@ -1,11 +1,12 @@
 function showCross(){
-		$('body').append('<img id="something" src="resources/images/cross_mark.png" alt="cross" width="100px" height="100px">');
+	$('body').append('<img id="something" src="resources/images/cross_mark.png" alt="cross" width="100px" height="100px">');
 
-		var div = $('#something');
+	var div = $('#something');
 
-	    div.animate({opacity: '0.2'}, "slow");
-    	div.animate({opacity: '1.0'}, "slow");
-	}
+    div.animate({opacity: '0.2'}, "slow");
+	div.animate({opacity: '1.0'}, "slow");
+	div.remove();
+}
 
 function showCheck(){
 	$('body').append('<img id="something" src="resources/images/check_mark.png" alt="cross" width="100px" height="100px">');
@@ -14,4 +15,6 @@ function showCheck(){
 
     div.animate({opacity: '0.2'}, "slow");
 	div.animate({opacity: '1.0'}, "slow");
+	
+	div.remove();
 }
