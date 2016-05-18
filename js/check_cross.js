7f959c7164bb7c36e5e473ce9eb42017fe1530bf
Shawn Kim <sdf1342@gmail.com>
2016-05-18 13:57:19 -0700

    check_cross.js

--- js/check_cross.js
+++ js/check_cross.js
@@ -1,11 +1,12 @@
 function showCross(){
-	var cross = $('<img id="cross" src="resources/images/cross_mark.png" alt="cross" width="100px" height="100px">');
+	var cross = '<img id="cross" src="resources/images/cross_mark.png" alt="cross" width="100px" height="100px">';
 
 	//cross.css("position", "fixed");
 	console.log("cross");
 
 	$('#topLayer').append(cross);
 
+	cross = $('#cross');
     cross.animate({opacity: '0.2'}, "slow");
 	cross.animate({opacity: '1.0'}, "slow", function(){
 		cross.remove();
@@ -13,13 +14,14 @@ function showCross(){
 }
 
 function showCheck(){
-	var check = $('<img id="check" src="resources/images/check_mark.png" alt="cross" width="100px" height="100px">');
+	var check = '<img id="check" src="resources/images/check_mark.png" alt="cross" width="100px" height="100px">'\;
 	
 	//check.css("position", "fixed");
 	console.log("check");
 
 	$('#topLayer').append(check);
 
+	check = $('#check');
     check.animate({opacity: '0.2'}, "slow");
 	check.animate({opacity: '1.0'}, "slow", function(){
 		check.remove();
