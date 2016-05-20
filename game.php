161f088df688642202aabd89dc264969052c70ef
Shawn Kim <sdf1342@gmail.com>
2016-05-19 18:27:52 -0700

    check mark popup

--- game.php
+++ game.php
@@ -17,6 +17,11 @@
 
 		<?php include('menu_button.php'); ?>
 
+		<div id='topLayer'>
+			<!-- <img id="cross" class='feedback' src="resources/images/cross_mark.png" alt="cross"> -->
+			<img id="check" class='feedback' src="resources/images/check_mark.png" alt="check" >
+		</div>
+		
 		<!--Progress Bar-->
 		<div id="progressBar">
 		</div>
@@ -32,8 +37,4 @@
 		<!-- Skip Button -->
 		<input type='button' class='btn skipBtn' value='Skip' onclick='skip()'>
 
-		<div id='topLayer'>
-			<!-- <img id="cross" class='feedback' src="resources/images/cross_mark.png" alt="cross"> -->
-			<img id="check" class='feedback' src="resources/images/check_mark.png" alt="check" >
-		</div>
 <?php include('footer.php'); ?>
