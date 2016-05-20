3fa80891e6af77c89b4ad6efafbe0a48cac41ba8
Brayden Traas <btraas@gmail.com>
2016-05-19 19:22:32 -0700

    variable grid size

--- js/game.js
+++ js/game.js
@@ -19,7 +19,7 @@ gameMode = 1;
 //gameMode = getCookie("gameMode");
 
 // set matrix width / height by window width
-matrixSize = $(window).width() - 50px;
+matrixSize = $(window).width() * 2;
 
 
 // Math to determine elements sizes
