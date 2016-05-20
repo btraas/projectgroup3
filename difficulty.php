2ebc410d1237f47a624ad718241dfbdece591627
BenjaminHao <haomingkai@gmail.com>
2016-05-19 17:21:38 -0700

    added chanllenge button

--- difficulty.php
+++ difficulty.php
@@ -65,6 +65,13 @@
 		<?php include('menu_button.php'); ?>
 
         <div id="text"><label for="level">Difficulty:</label> </div>
+
+            <fieldset data-role="controlgroup"  data-type="horizontal" data-role="fieldcontain">
+            <input type="radio" name="radio-choice-a1" id="radio-choice-a1" value="0"  onclick=""/>
+			<label for="radio-choice-a1">Classic</label>
+			<input type="radio" name="radio-choice-a1" id="radio-choice-b1" value="1"  onclick=""/>
+			<label for="radio-choice-b1">Challenge</label>
+            </fieldset>
                
 	    <div class="lvimg">
         		<img id="lvImg" src="resources/level/Lv1(fake).png" alt="Level Image" height="280" width="280">
