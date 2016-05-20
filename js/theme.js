dfcd58cbb751568cb17a73f28c14569e31ef441a
BenjaminHao <haomingkai@gmail.com>
2016-05-19 19:41:34 -0700

    1

--- js/theme.js
+++ js/theme.js
@@ -2,5 +2,6 @@ function Dtheme(){
     document.getElementById('theme').href = "css/themes/darkTheme.min.css";
 }
 function Ltheme(){
-    document.getElementById('theme').href = "css/themes/lightTheme.min.css"; 
+    document.getElementById('theme').href = "css/themes/lightTheme.min.css";
+     
 }
