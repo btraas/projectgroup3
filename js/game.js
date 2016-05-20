eeacae02e6b6f93684ca5cb8fbcbaf31c4676a9b
Shawn Kim <sdf1342@gmail.com>
2016-05-19 18:09:40 -0700

    userProgress()

--- js/game.js
+++ js/game.js
@@ -247,10 +247,7 @@ function showUserProgress(){
 		}
 		var progressNum = document.createElement("div");
 		progressNum.id = "progressNumber";
-
-		if(progressNum != null){
 		progressBar.appendChild(progressNum);
-		}
 	}
 
 	//check user progress and visualizing based on userProgress array
