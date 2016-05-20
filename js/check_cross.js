eebb2b80e0fdda92dd2e1690f0f85e4e5f28ca71
Shawn Kim <sdf1342@gmail.com>
2016-05-19 19:30:58 -0700

    check()

--- js/check_cross.js
+++ js/check_cross.js
@@ -15,9 +15,10 @@ function showCheck(){
 
 	check = $('#check');
 
-	check.animate({top: '+50%'},"slow");
+	check.animate({top: '+=50%'},"slow");
 	check.animate({opacity: '1.0'}, "slow");
-	check.animate({top: '-50%'},"slow");
+	check.animate({top: '-=50%'},"slow");
+	check.animate({opacity: '0.0'}, "slow");
 	
 	// check.animate({opacity: '1.0'}, "slow", function(){
 	// 	check.animate({opacity:'0.0'}, "slow");
