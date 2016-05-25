5ab0fbeef1d64fe85a2e1b40744d92f7c23b4137
kelvin5929 <kelvin5929@gmail.com>
2016-05-25 13:56:11 -0700

    added script to create a member table

--- sql/generateDB.sql
+++ sql/generateDB.sql
@@ -13,4 +13,13 @@ CREATE TABLE leaderboards (
   FOREIGN KEY (username) REFERENCES username(id) ON DELETE CASCADE
 ) ENGINE=MyISAM;
 
+CREATE TABLE members (
+  id INTEGER NOT NULL auto_increment,
+  username varchar(255) default '' NOT NULL ,
+  password varchar(255) default '' NOT NULL ,
+  datetime varchar(25) NOT NULL default '',
+  PRIMARY KEY (id),
+  FOREIGN KEY (username) REFERENCES username(id) ON DELETE CASCADE
+) ENGINE=MyISAM;
+
 
