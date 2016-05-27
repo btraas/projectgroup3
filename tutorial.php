02d446e7d7b48c862f60c4d26fb80156aff6be2c
Shawn Kim <sdf1342@gmail.com>
2016-05-27 02:13:08 -0700

    tutorial.php tutorial.js bxslider implementing

--- tutorial.php
+++ tutorial.php
@@ -2,17 +2,8 @@
 	include('header.php');
     session_start();
 ?>
-        <!-- bxSlider Javascript file -->
-        <script src="js/bxslider/jquery.bxslider.min.js"></script>
-        <script src="js/tutorial.js"></script>
-        <!-- bxSlider CSS file -->
-        <link href="css/jquery.bxslider.css" rel="stylesheet" />
 <?php include('menu_button.php'); ?>
 	<div id="index_body_header">
-		<ul class="bxslider1">
-            <!-- WEST -->
-            <li><img src="" alt="t01"></li>
-            <li><img src="" alt="t02"></li>
-        </ul>	
+    
     </div>
 <?php include('footer.php'); ?>
