5f0a655af23b6aa942332ac5314cd4b16439d54b
Shawn Kim <sdf1342@gmail.com>
2016-05-27 03:39:16 -0700

    implement youtube video testing for tutorial

--- tutorial.php
+++ tutorial.php
@@ -8,7 +8,7 @@
 
 	//exit the current bgm when they get out this page
 	$(document).on('pagebeforehide', "[data-url='/tutorial.php']", function(){
-	    BGM.volume(getCookie('bgm'));
+		BGM.next();
 	});
 </script>
 <?php include('menu_button.php'); ?>
