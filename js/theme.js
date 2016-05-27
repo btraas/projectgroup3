242cd41f1fdfd0af26d28c8481a98f9eefe83533
kelvin5929 <kelvin5929@gmail.com>
2016-05-26 23:22:43 -0700

    improved light theme

--- js/theme.js
+++ js/theme.js
@@ -5,6 +5,7 @@ function Dtheme(){
     document.getElementById('mainCSS').href = "css/main.css"; 
     document.getElementById('menuCSS').href = "css/menu.css"; 
     document.getElementById('settingsCSS').href = "css/settings.css";
+    document.getElementById('difficultyCSS').href = "css/difficulty.css"; 
     document.getElementById('patternCSS').href = "js/patternlock/patternLock.css"; 
     document.getElementById('gameCSS').href = "css/game.css";
     document.getElementById('resultCSS').href = "css/result.css";     
@@ -16,6 +17,7 @@ function Ltheme(){
     document.getElementById('mainCSS').href = "css/main_L.css";
     document.getElementById('menuCSS').href = "css/menu_L.css";  
     document.getElementById('settingsCSS').href = "css/settings_L.css";
+    document.getElementById('difficultyCSS').href = "css/difficulty_L.css"; 
     document.getElementById('patternCSS').href = "js/patternlock/patternLock_L.css"; 
     document.getElementById('gameCSS').href = "css/game_L.css";   
     document.getElementById('resultCSS').href = "css/result_L.css"; 
