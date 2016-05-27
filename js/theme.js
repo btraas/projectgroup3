ac79dd50223d05c39e67b9a496cfc36166bb934e
BenjaminHao <haomingkai@gmail.com>
2016-05-27 02:37:27 -0700

    fixed icon bug

--- js/theme.js
+++ js/theme.js
@@ -4,7 +4,6 @@ function Dtheme(){
     document.getElementById('theme').href = "css/themes/darkTheme.min.css";
     document.getElementById('mainCSS').href = "css/main.css"; 
     document.getElementById('menuCSS').href = "css/menu.css"; 
-    document.getElementById('settingsCSS').href = "css/settings.css";
     document.getElementById('difficultyCSS').href = "css/difficulty.css"; 
 }
 //Ligth theme replace the source of css root
@@ -12,7 +11,6 @@ function Ltheme(){
     document.getElementById('theme').href = "css/themes/lightTheme.min.css";
     document.getElementById('mainCSS').href = "css/main_L.css";
     document.getElementById('menuCSS').href = "css/menu_L.css";  
-    document.getElementById('settingsCSS').href = "css/settings_L.css";
     document.getElementById('difficultyCSS').href = "css/difficulty_L.css";
 }
 
