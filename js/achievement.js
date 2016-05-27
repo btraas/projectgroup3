66db10748d51f24bbe9bca0ca012a37cfe83a5d6
Shawn Kim <sdf1342@gmail.com>
2016-05-26 23:04:59 -0700

    achievement.js fixing looping out

--- js/achievement.js
+++ js/achievement.js
@@ -1,6 +1,6 @@
 //achievement.js
 //exit the current bgm when they get out this page
-$(document).on('pagebeforehide', "[data-url='/achievment.php']", function()
+$(document).on('pagebeforehide', "[data-url='/achievement.php']", function()
 {
 	BGM.next();
 });
