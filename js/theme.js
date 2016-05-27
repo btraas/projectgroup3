c240ca3ea984ea4ee51fdcdcdd38caf9b41594f6
BenjaminHao <haomingkai@gmail.com>
2016-05-26 17:47:40 -0700

    more light theme

--- js/theme.js
+++ js/theme.js
@@ -4,6 +4,7 @@ function Dtheme(){
     document.getElementById('mainCSS').href = "css/main.css"; 
     document.getElementById('menuCSS').href = "css/menu.css"; 
     document.getElementById('settingsCSS').href = "css/settings.css";
+    document.getElementById('difficultyCSS').href = "css/difficulty.css"; 
     document.getElementById('gameCSS').href = "css/game.css";
     document.getElementById('resultCSS').href = "css/result.css";     
     document.getElementById('leaderboardsCSS').href = "css/leaderboards.css"; 
@@ -12,8 +13,9 @@ function Dtheme(){
 function Ltheme(){
     document.getElementById('theme').href = "css/themes/lightTheme.min.css";
     document.getElementById('mainCSS').href = "css/main_L.css";
-     document.getElementById('menuCSS').href = "css/menu_L.css";  
+    document.getElementById('menuCSS').href = "css/menu_L.css";  
     document.getElementById('settingsCSS').href = "css/settings_L.css";
+    document.getElementById('difficultyCSS').href = "css/difficulty_L.css"; 
     document.getElementById('gameCSS').href = "css/game_L.css";   
     document.getElementById('resultCSS').href = "css/result_L.css"; 
     document.getElementById('leaderboardsCSS').href = "css/leaderboards_L.css";
