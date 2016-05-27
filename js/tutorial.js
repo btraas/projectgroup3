1731f692bea7a7a95f4360a4abe17e58768ac6a9
Shawn Kim <sdf1342@gmail.com>
2016-05-27 03:30:40 -0700

    implement youtube video testing for tutorial

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,7 +1,7 @@
 //tutorial.js
+BGM.volume(0);
 
 //exit the current bgm when they get out this page
-$(document).on('pagebeforehide', "[data-url='/tutorial.php']", function()
-{
-    BGM.volume(100);
+$(document).on('pagebeforehide', "[data-url='/tutorial.php']", function(){
+    BGM.volume(getCookie('bgm'));
 });
--- tutorial.php
+++ tutorial.php
@@ -3,10 +3,8 @@
     session_start();
 ?>
 <link rel="stylesheet" href="css/tutorial.css" />
+<script src="js/tutorial.js"></script>
 <?php include('menu_button.php'); ?>
-<script>
-	BGM.volume(0);
-</script>
 	<div class="tutorial" id="tutorial_video">
     	<iframe src="https://www.youtube.com/embed/OXWrjWDQh7Q"></iframe>
 	</div>
