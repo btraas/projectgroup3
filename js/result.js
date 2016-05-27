e009cfe1b4a81d6bef1a17031cf8be6c8b508a23
Shawn Kim <sdf1342@gmail.com>
2016-05-26 21:55:56 -0700

    result script moved into .js and add move sounds

--- js/result.js
+++ js/result.js
@@ -1,6 +1,9 @@
 // result.js
 
 // Posts score to server, also saves user score locally
+var rank = <?php echo $rows['rank']; ?>;
+var score = <?php echo $score; ?>;
+BGM.loop('resources/sounds/bgm_scoreboard.mp3');
 
 // direct to other pages accordingly
 $(document).on('pageshow', "[data-url^='/result.php']", function(){
@@ -9,7 +12,8 @@ $(document).on('pageshow', "[data-url^='/result.php']", function(){
 	} else {
 		$('.postBtn').attr("src", "resources/images/result_buttons_leaderboards_post.png");
 	}
-    
+
+	BGM.next();
 });
 
 function goPost()  {
--- result.php
+++ result.php
@@ -28,10 +28,11 @@
 
 <!-- This needs to be in this PHP file to set these variables -->
 <script>
-	BGM.loop('resources/sounds/bgm_scoreboard.mp3');
+	// BGM.loop('resources/sounds/bgm_scoreboard.mp3');
 
-	var rank = <?php echo $rows['rank']; ?>;
-	var score = <?php echo $score; ?>;
+	// var rank = <?php echo $rows['rank']; ?>;
+	// var score = <?php echo $score; ?>;
+
 </script>
     <script src='js/theme.js'></script>
 	<link id="resultCSS" rel="stylesheet" type="text/css" href="css/result.css">
