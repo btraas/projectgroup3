80313f49711149fda3000fd4c80ddfdc98d79860
Shawn Kim <sdf1342@gmail.com>
2016-05-27 03:32:59 -0700

    #tutorial

--- tutorial.php
+++ tutorial.php
@@ -3,7 +3,15 @@
     session_start();
 ?>
 <link rel="stylesheet" href="css/tutorial.css" />
-<script src="js/tutorial.js"></script>
+<script>
+	BGM.volume(0);
+
+	//exit the current bgm when they get out this page
+	$(document).on('pagebeforehide', "[data-url='/tutorial.php']", function(){
+	    BGM.volume(getCookie('bgm'));
+	});
+=
+</script>
 <?php include('menu_button.php'); ?>
 	<div class="tutorial" id="tutorial_video">
     	<iframe src="https://www.youtube.com/embed/OXWrjWDQh7Q"></iframe>
