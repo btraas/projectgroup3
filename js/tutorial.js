7f3927367e8c5288f4388f4862f1e81ef3cb068b
Shawn Kim <sdf1342@gmail.com>
2016-05-27 01:45:08 -0700

    #bxslider, tutorial.js, jquery.bxslider.css

---
+++ js/tutorial.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+  $('.bxslider1').bxSlider({
+    slideWidth: 200,
+    slideHeight: 100,
+    minSlides: 5,
+    maxSlides: 5,
+    slideMargin: 10,
+    randomStart: true,
+    controls: true,
+    responsive: false,
+    auto:true
+  });
+});
