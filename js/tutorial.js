c38d957334c8f04925ac4f10696aa7e47718f1d4
Shawn Kim <sdf1342@gmail.com>
2016-05-27 01:47:05 -0700

    tutorial.php tutorial.js bxslider implementing

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,3 +1,4 @@
+//tutorial.js
 
 $(document).ready(function(){
   $('.bxslider1').bxSlider({
--- tutorial.php
+++ tutorial.php
@@ -11,21 +11,8 @@
 	<div id="index_body_header">
 		<ul class="bxslider1">
             <!-- WEST -->
-            <li style="height: 110px"><a id="map_portland" href="torontoRaptors.php"><img src="images/teamlogo/portland_trail_blazers.gif" alt=""></a></li>
-            <li><a id="map_utah" href="teams_utah_jazz.php"><img src="images/teamlogo/utah_jazz.gif" alt=""></a></li>
-            <li><a id="map_denver" href="teams_denver_nuggets.php"><img src="images/teamlogo/denver_nuggets.gif" alt=""></a></li>
-            <li><a id="map_oklahoma_city" href="teams_oklahoma_city_thunder.php"><img src="images/teamlogo/oklahoma_city_thunder.gif" alt=""></a></li>
-            <li><a id="map_minnesota" href="teams_minnesota_timberwolves.php"><img src="images/teamlogo/minnesota_timberwolves.gif" alt=""></a></li>
-            <li><a id="map_sacramento" href="teams_sacramento_kings.php"><img src="images/teamlogo/sacramento_kings.gif" alt=""></a></li>
-            <li><a id="map_golden_state" href="teams_golden_state_warriors.php"><img src="images/teamlogo/golden_state_warriors.gif" alt=""></a></li>
-            <li><a id="map_la_lakers" href="teams_los_angeles_lakers.php"><img src="images/teamlogo/los_angeles_lakers.gif" alt=""></a></li>
-            <li><a id="map_la_clippers" href="teams_los_angeles_clippers.php"><img src="images/teamlogo/los_angeles_clippers.gif" alt=""></a></li>
-            <li><a id="map_phoenix" href="teams_phoenix_suns.php"><img src="images/teamlogo/phoenix_suns.gif" alt=""></a></li>
-            <li><a id="map_dallas" href="teams_dallas_mavericks.php"><img src="images/teamlogo/dallas_mavericks.gif" alt=""></a></li>
-            <li><a id="map_san_antonio" href="teams_san_antonio_spurs.php"><img src="images/teamlogo/san_antonio_spurs.gif" alt=""></a></li>
-            <li><a id="map_houston" href="teams_houston_rockets.php"><img src="images/teamlogo/houston_rockets.gif" alt=""></a></li>
-            <li><a id="map_new_orleans" href="teams_new_orleans_pelicans.php"><img src="images/teamlogo/new_orleans_pelicans.gif" alt=""></a></li>
-            <li><a id="map_memphis" href="teams_memphis_grizzlies.php"><img src="images/teamlogo/memphis_grizzlies.gif" alt=""></a></li>
+            <li><img src="" alt="t01"></li>
+            <li><img src="" alt="t02"></li>
         </ul>	
     </div>
 <?php include('footer.php'); ?>
