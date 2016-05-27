703b2195798da6ea96a5db4461014e40ea8fda90
Shawn Kim <sdf1342@gmail.com>
2016-05-26 23:07:15 -0700

    achievement.js fixing looping out

--- js/achievement.js
+++ js/achievement.js
@@ -1,4 +1,11 @@
 //achievement.js
+
+//play sounds
+$(document).on('pagebeforeshow', "[data-url='/achievement.php']", function()
+{
+	BGM.loop('resources/sounds/bgm_eastern_valley.mp3');
+});
+
 //exit the current bgm when they get out this page
 $(document).on('pagebeforehide', "[data-url='/achievement.php']", function()
 {
