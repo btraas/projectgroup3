9fdffb9d287d017821a8885de35856270cf81778
kelvin5929 <kelvin5929@gmail.com>
2016-05-27 00:01:18 -0700

    fixed bugs

--- config.php
+++ config.php
@@ -6,23 +6,17 @@
     define('HOMEURL', 'index.php');
 
 
-    //Function to sanitize values received from the form. Prevents SQL injection
-    function clean($str) {
-        $str = @trim($str);
-        if(get_magic_quotes_gpc()) {
-            $str = stripslashes($str);
-        }
-        return mysql_real_escape_string($str);
+//Function to sanitize values received from the form. Prevents SQL injection
+function clean($str) {
+    $str = @trim($str);
+    if(get_magic_quotes_gpc()) {
+        $str = stripslashes($str);
     }
+    return mysql_real_escape_string($str);
+}
 
 function runQ($query)
 {
-	//Array to store validation errors
-    $errmsg_arr = array();
-
-    //Validation error flag
-    $errflag = false;
-
     //Connect to mysql server
     $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
     if(!$link) {
@@ -34,7 +28,6 @@ function runQ($query)
     if(!$db) {
         die("Unable to select database");
     }
-
 
     $result=mysql_query($query);
 	if(!$result) die('Query failed');
